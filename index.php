bd39910027e5520ed4dc177be0a42068abf6e272
Matthieu <matthieusauval@gmail.com>
2019-04-29 14:39:10 +0200

    bonhomme

--- index.php
+++ index.php
@@ -8,11 +8,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
     <body>
-
-    <header>dodo</header>
-        <h1>Viva Neymar !!!!!!!!!!</h1>
-        <p>Plouf</p>
-        <h1>Viva Flo Tov'''</h1>
-        <h2>ETZJHRETGHSO</h2>
+        <div id="tete"> </div>
+        <div id="corp"> </div>
+        <div id="brasd"> </div>
+        <div id="brasg"> </div>
+        <div id="jambed"> </div>
+        <div id="jambeg"> </div>
+        <div id="shoesd"> </div>
+        <div id="shoesg"> </div>
+        <div id="bouled"> </div>
+        <div id="bouleg"> </div>
+        <div id="tige"> </div>
     </body>
 </html>
