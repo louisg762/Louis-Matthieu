21beaeab726fb4cff724dbd994d59aa68a79d358
Matthieu <matthieusauval@gmail.com>
2019-04-29 14:30:28 +0200

    modif header

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>allez Paris</title>
+    <link rel="stylesheet" href="style.css">
 </head>
     <body>
+
+    <header>dodo</header>
         <h1>Viva Neymar !!!!!!!!!!</h1>
         <p>Plouf</p>
         <h1>Viva Flo Tov'''</h1>
